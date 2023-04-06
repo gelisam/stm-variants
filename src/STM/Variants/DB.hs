2754fd7ba1dcb0f5e95c8ff4f69f9034f7bc87a2
Samuel Gélineau <gelisam+github@gmail.com>
2023-04-05 23:26:52 -0400

    LevelDB example

--- src/STM/Variants/DB.hs
+++ src/STM/Variants/DB.hs
@@ -1,4 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | A database with an API which closely matches the "Control.Concurrent.STM"
 -- API.
 module STM.Variants.DB where
 
+import qualified Database.LevelDB as LevelDB
+
+
+test :: IO ()
+test = do
+  (value1, value2) <- LevelDB.runResourceT $ do
+    db <- LevelDB.open "example.db" (LevelDB.defaultOptions { LevelDB.createIfMissing = True })
+    LevelDB.put db LevelDB.defaultWriteOptions "key1" "value1"
+    LevelDB.put db LevelDB.defaultWriteOptions "key2" "value2"
+    value1 <- LevelDB.get db LevelDB.defaultReadOptions "key1"
+    value2 <- LevelDB.get db LevelDB.defaultReadOptions "key2"
+    pure (value1, value2)
+  print value1
+  print value2
